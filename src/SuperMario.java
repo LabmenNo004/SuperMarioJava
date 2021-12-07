4bcc4fa9f71e7098fe81c97288172852d8ef74bc
Zehao Fan <zehao@10-18-60-26.dynapool.wireless.nyu.edu>
2021-12-07 16:00:21 -0500

    add key control

--- src/SuperMario.java
+++ src/SuperMario.java
@@ -1,51 +1,57 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class SuperMario{
-	//	everything static, since there is only one game.
+public class SuperMario {
+	// everything static, since there is only one game.
 	private static final int WIDTH = 1200;
 	private static final int HEIGHT = 900;
 	private static int stageNumber = 1;
 	private static int marioLives = 3;
 	private static int score = 0;
 	private static JFrame frame;
+	private static boolean leftPressed;
+	private static boolean rightPressed;
+	private static boolean jumpPressed;
+
 	public SuperMario() {
-		//create frame
+		// create frame
 		frame = new JFrame();
-		frame.setSize(WIDTH,HEIGHT);
+		frame.setSize(WIDTH, HEIGHT);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		
+
+		frame.addKeyListener(new CustomKeyListener());
+
 		gameStart();
 	}
-	
+
 	public static JPanel getStartPanel() {
 //		needs implement
 		// initial frame
 		return null;
 	}
-	
-	
+
 	public static void gameStart() {
 //		needs implement
 	}
-	
+
 	public static void increaseScore(int number) {
 		score += number;
 	}
-	
+
 	public static void enterStage() {
 		Stage stage = new Stage(stageNumber);
 		frame.add(stage);
 	}
-	
+
 	public static void gameOver() {
 //		show game over view, showing score. 
 //		needs implement
-		
-		
+
 //		restore data
 		stageNumber = 1;
 		marioLives = 3;
@@ -53,22 +59,59 @@ public class SuperMario{
 //		restart
 		gameStart();
 	}
-	
+
 	public static void loseLive() {
 		if (marioLives == 1) {
 			gameOver();
-		}else {
+		} else {
 			marioLives--;
 			gameStart();
 		}
 	}
-	
+
+	public static boolean isLeftPressed() {
+		return leftPressed;
+	}
+
+	public static boolean isRightPressed() {
+		return rightPressed;
+	}
+
+	public static boolean isJumpPressed() {
+		return jumpPressed;
+	}
+
 	public static int getWIDTH() {
 		return WIDTH;
 	}
-	
-	public static void main(String[] args)  {
+
+	class CustomKeyListener implements KeyListener {
+		public void keyTyped(KeyEvent e) {
+		}
+
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				leftPressed = true;
+			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rightPressed = true;
+			} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				jumpPressed = true;
+			}
+		}
+
+		public void keyReleased(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				leftPressed = false;
+			} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				rightPressed = false;
+			} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				jumpPressed = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
 		SuperMario game = new SuperMario();
-		
+
 	}
 }
