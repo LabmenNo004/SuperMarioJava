b4e0f6d658eb208d41825181d99d2a9e8b26d9c1
苏季鹏 <sujipeng@sujipengdeMBP.cable.rcn.com>
2021-12-08 13:21:34 -0500

    1

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -5,12 +5,30 @@ public class KeyInput implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
+		int key = e.getKeyCode();  
+		for (Charactor ch:Handler.charactor) {
+		switch(key) {
+		case KeyEvent.VK_W:
+			setVelY(-5);
+			break;
+		case KeyEvent.VK_S:
+			setVelY(5);
+			break;
+		case KeyEvent.VK_A:
+			setVelX(-5);
+			break;
+		case KeyEvent.VK_D:
+			setVelX(5);
+			break;
+		}
+		}
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		int key = e.getKeyCode();  
 		
 	}
 
